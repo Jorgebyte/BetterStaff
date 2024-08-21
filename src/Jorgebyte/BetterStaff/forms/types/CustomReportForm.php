d8f71dab130bde74b67c5eebea44d102a63045c9
Jorge <134261497+Jorgebyte@users.noreply.github.com>
2024-08-20 18:26:29 -0600

    Update CustomReportForm.php

--- src/Jorgebyte/BetterStaff/forms/types/CustomReportForm.php
+++ src/Jorgebyte/BetterStaff/forms/types/CustomReportForm.php
@@ -11,17 +11,22 @@ use Vecnavium\FormsUI\CustomForm;
 
 class CustomReportForm extends CustomForm
 {
-    private array $playerNames = [];
-
     public function __construct()
     {
-        parent::__construct(function (Player $player, ?array $data) {
+        $onlinePlayers = Server::getInstance()->getOnlinePlayers();
+        $playerNames = [];
+
+        foreach ($onlinePlayers as $onlinePlayer) {
+            $playerNames[] = $onlinePlayer->getName();
+        }
+
+        parent::__construct(function (Player $player, ?array $data) use ($playerNames) {
             if ($data === null) {
                 return;
             }
 
             $selectedPlayerIndex = (int)$data[0];
-            $reportedPlayerName = $this->playerNames[$selectedPlayerIndex];
+            $reportedPlayerName = $playerNames[$selectedPlayerIndex];
             $reportReason = $data[1];
 
             if ($reportedPlayerName === "" || $reportReason === "") {
@@ -41,16 +46,12 @@ class CustomReportForm extends CustomForm
                     SoundUtils::addSound($staff, "random.pop");
                 }
             }
+
             WebhookUtils::sendReportWebhook($player->getName(), $reportedPlayerName, $reportReason);
         });
 
         $this->setTitle("Report");
-        $this->addInput("Reason for the report");
-    }
-
-    public function setPlayerNames(array $playerNames): void
-    {
-        $this->playerNames = $playerNames;
         $this->addDropdown("Select a player to report", $playerNames);
+        $this->addInput("Reason for the report");
     }
 }
